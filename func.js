d01a486f2af5f1b70030a33c257f5c95c6d85c27
prathima129 <prathima.shrestha@gmail.com>
2025-06-12 06:50:26 +0000

    Done with Functions(with Parameters)

---
+++ func.js
@@ -0,0 +1,82 @@
+/*
+-Functions are one of the most powerful features in JS .
+-They allow to reuse the code,organize logic,and handle dynamic inputs
+-Functions are used always like
+   declaration + call
+*/
+   
+//-Syntax
+  //For function declaration
+
+function sayHello(){
+    console.log("Hello")
+}
+
+//For function call
+sayHello()
+
+//the function will declare only when we call the function without calling function the declaration is of no use
+
+/*console.log(a)
+let a=12; */
+
+//let us take one example
+
+sayHello()
+
+function sayHello(){
+    console.log("Hello")
+}
+
+//Functions with Parameters
+
+function sum() {
+    console.log(10+6)
+}
+sum()
+
+//if user have to give the values then the code will be
+
+function sum(num1,num2) {
+    console.log(num1+num2)
+}
+sum(2,4) 
+
+function sum(num1,num2,num3) {
+    console.log(num1+num2+num3)
+}
+sum(2,4,13)
+
+function sub() {
+    console.log(10-6)
+}
+sub()
+
+function mul() {
+    console.log(10*6)
+}
+mul()
+
+function div() {
+    console.log(10/6)
+}
+div()
+
+function sub(num1,num2) {
+    console.log(num1-num2)
+}
+sub(2,4) 
+
+function mul(num1,num2) {
+    console.log(num1*num2)
+}
+mul(2,4) 
+
+function div(num1,num2) {
+    console.log(num1/num2)
+}
+div(2,4) 
+
+
+
+
